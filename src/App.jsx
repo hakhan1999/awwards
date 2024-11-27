9c6bf1ba66a230ca8780138e39d768e658877f8d
hakhan1999 <khanhassaan961@gmail.com>
2024-11-27 16:40:38 +0500

    Navbar Completed and Lenis Smooth Scroll Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Lenis from "lenis";
+import "lenis/dist/lenis.css";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
 
 const App = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      autoRaf: true,
+    });
+  });
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
       <Hero />
       <About />
     </main>
